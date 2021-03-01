8631aa0c2a9b5336d3508a725dae1e5de5012b07
Naresh <nareshpathivada8@gmail.com>
2021-03-01 15:22:21 +0530

    commit

--- first project.py
+++ first project.py
@@ -1,1 +1,7 @@
-print("hiii")
+word =str(input())
+print("enter a word")
+keywords = ("break","case","default","continue","defer","else","for","func","goto","if","map","range","return","struct","type","var")
+if word in keywords:
+    print(word +"  is a keyword. ")
+else:
+    print(word +" is not a keyword. ")
